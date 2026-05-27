8e4d7334ea3b4ed6153f621cf60802632d119359
ICE <kingsley.aremu@gmail.com>
2026-05-27 13:07:24 +0100

    Update snow toggle icon

--- components/ReactBits/SnowToggle.js
+++ components/ReactBits/SnowToggle.js
@@ -163,8 +163,8 @@ const SnowToggle = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
@@ -173,12 +173,18 @@ const SnowToggle = () => {
           strokeLinejoin="round"
           className={`transition-all duration-300 ${isSnowing ? 'opacity-100' : 'opacity-40 group-hover:opacity-100'}`}
         >
-          <line x1="2" y1="12" x2="22" y2="12" />
-          <line x1="12" y1="2" x2="12" y2="22" />
-          <path d="m20 20-4.75-4.75" />
-          <path d="m4 4 4.75 4.75" />
-          <path d="m4 20 4.75-4.75" />
-          <path d="m20 4-4.75 4.75" />
+          <path d="m10 20-1.25-2.5L6 18" />
+          <path d="M10 4 8.75 6.5 6 6" />
+          <path d="m14 20 1.25-2.5L18 18" />
+          <path d="m14 4 1.25 2.5L18 6" />
+          <path d="m17 21-3-6h-4" />
+          <path d="m17 3-3 6 1.5 3" />
+          <path d="M2 12h6.5L10 9" />
+          <path d="m20 10-1.5 2 1.5 2" />
+          <path d="M22 12h-6.5L14 15" />
+          <path d="m4 10 1.5 2L4 14" />
+          <path d="m7 21 3-6-1.5-3" />
+          <path d="m7 3 3 6h4" />
         </svg>
       </button>
 
